e3df63f92b7d427c08fa92aae2d9c80a5c25b0a0
rjchu@uwaterloo.ca <ubuntu@ip-172-31-20-218.us-east-2.compute.internal>
2020-10-15 21:59:32 +0000

    fix stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,7 +4,4 @@ Rails.configuration.stripe = {
   secret_key: ENV['STRIPE_TEST_SECRET_KEY']
 }
 
-#Stripe.api_key = Rails.configuration.stripe[:secret_key]
-#Stripe.api_key = Rails.configuration.stripe[:STRIPE_TEST_SECRET_KEY]
-#Stripe.api_key = ENV['STRIPE_TEST_SECRET_KEY']
-Stripe.api_key = 'sk_test_51HbHRKFwKTuf9zuWx3eQnHySia9biz9VE0kDXIXGZxMqDBMbnTDlUgmp1vzUgWQ9dsbkecNW31hyEwUdXzzhMd5K005OyN41Ez'
+Stripe.api_key = Rails.application.credentials[:STRIPE_TEST_SECRET_KEY]
