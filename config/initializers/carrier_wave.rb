e3a6bcebdee4a34a6592e353fda8274331fb5a2e
rjchu@uwaterloo.ca <ubuntu@ip-172-31-20-218.us-east-2.compute.internal>
2020-10-15 22:47:22 +0000

    image upload

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,14 @@ if Rails.env.production?
     CarrierWave.configure do |config|
         config.fog_credentials = {
         :provider => 'AWS',
+        :aws_access_key_id => Rails.application.credentials[:access_key_id],
+        :aws_secret_access_key => ENRails.application.credentials[:secret_access_key]
+
         #:aws_access_key_id => ENV['S3_ACCESS_KEY'],
-        :aws_access_key_id => 'AKIAQGYCIAF4OKEMIL6Y',
         #:aws_secret_access_key => ENV['S3_SECRET_KEY']
-        :aws_secret_access_key => '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'
+        #:aws_secret_access_key => '9olz3uapLMvj5goODNL6KzVQKr7YUQ0T76//vSCk'
         }
+        #config.fog_directory = ENV['S3_BUCKET']
         #config.fog_directory = ENV['S3_BUCKET']
         config.fog_directory = 'photo-app-jimmy'
     end
