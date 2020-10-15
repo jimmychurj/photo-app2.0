0e753d28432590e20fe7ef3379e7c29eb500cf91
rjchu@uwaterloo.ca <ubuntu@ip-172-31-20-218.us-east-2.compute.internal>
2020-10-15 21:00:06 +0000

    enable master key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,4 +5,5 @@ Rails.configuration.stripe = {
 }
 
 #Stripe.api_key = Rails.configuration.stripe[:secret_key]
-Stripe.api_key = 'pk_test_51HbHRKFwKTuf9zuWV3ebhTWwEX8oggvi0M9yL99BYTaswnKT3co4ve1W1bys0bmXsWRtsaZaQO3v2KsPWxdOFbS700zjabf0ts'
+Stripe.api_key = ENV['STRIPE_TEST_SECRET_KEY']
+#Stripe.api_key = 'pk_test_51HbHRKFwKTuf9zuWV3ebhTWwEX8oggvi0M9yL99BYTaswnKT3co4ve1W1bys0bmXsWRtsaZaQO3v2KsPWxdOFbS700zjabf0ts'
